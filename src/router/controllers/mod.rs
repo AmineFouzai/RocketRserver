c32feaff05b0e94dee8dd7e8a14bd5e6f7f4c7d8
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2021-03-16 16:50:41 +0100

    Update mod.rs

--- src/router/controllers/mod.rs
+++ src/router/controllers/mod.rs
@@ -24,7 +24,6 @@ pub fn qr_code_generator() -> String {
 
 #[get("/bar")]
 pub fn bar_code_generator() -> String {
-    //add request data to generate  codabar from  any dynamic data
 
     let barcode = EAN13::new("750103131130").unwrap();
     let encoded = barcode.encode().iter().map(|c| *c as char).collect::<Vec<_>>();
@@ -46,47 +45,5 @@ pub fn ocr_extractor() -> String {
 
 
 
-#[get("/resize")]
-pub fn resize_image() -> &'static str {
-    //todo
-    "image resizing "
-}
 
 
-
-#[get("/slogan")]
-pub fn slogan_generator() -> &'static str {
-    "generate slogan"
-}
-
-
-#[get("/business")]
-pub fn business_name_generator() -> &'static str {
-    "bessnins generator"
-}
-
-
-#[get("/detect")]
-pub fn face_detection_marker() -> &'static str {
-    "Hello, world 3!"
-}
-
-
-#[post("/upload",data = "<data>")]
-fn upload(content_type: &ContentType, data: MultipartDatas) -> Html<String>
-{
-    for f in data.files {
-        println!( "{}",format!("FieldName:{} --- FileName:{} --- StoragePath:{}<br>",
-                         f.name,f.filename,f.path));
-
-        f.persist(Path::new("upload"));
-    }
-    Html(format!("<html><head></head><body>upload coming...<br>{}</body></html>","ok"))
-}
-
-#[get("/download")]
-pub fn file_downloader() -> &'static str {
-    "Hello, world 3!"
-}
-
-//map this then to graphql
